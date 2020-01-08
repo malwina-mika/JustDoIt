090093c8da8f1fb515ed13596069a3bb02ebbdfd
Malwina8 <48411653+Malwina8@users.noreply.github.com>
2020-01-08 23:28:15 +0100

    added functionality to navbar - toggling pages

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,140 @@
-function toggleMenu(visible) {
-  document.querySelector('.navlinks').classList.toggle('show', visible);
-  document.querySelector('.avatar-wrapper').classList.toggle('show', visible);
+import {select, classNames} from './settings.js';
 
-}
+const app = {
 
-function toggleTopBar(visible) {
-    document.querySelector('.top-side').classList.toggle('display', visible);
-}
+  init: function () {
+    const thisApp = this;
 
-document.querySelector('.hamburger').addEventListener('click', function(e) {
-  e.preventDefault();
+    thisApp.initPages();
+    thisApp.initActions();
 
-  toggleMenu();
-  console.log("clicked!");
-});
+  },
 
-document.querySelector('.hamburger').addEventListener('click', function(e) {
-  e.preventDefault();
+  initPages: function() {
+    const thisApp = this;
 
-  toggleTopBar();
-  console.log("clicked2!");
-});
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    console.log('pages', thisApp.pages);
+
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    console.log('navlinks', thisApp.navLinks);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+    console.log('idFromHash', idFromHash);
+
+    let pageMatchingHash = thisApp.pages[0].id;
+    console.log('pageMatchingHash', pageMatchingHash);
+
+    for(let page of thisApp.pages){
+      console.log('page', page);
+      if(page.id === idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for(let link of thisApp.navLinks){
+
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        thisApp.activatePage(id);
+
+        window.location.hash = `#/${id}`;
+
+      });
+    }
+
+  },
+
+  activatePage: function(pageId) {
+    const thisApp = this;
+
+    for(let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(classNames.nav.active, link.getAttribute('href') == `#${pageId}`);
+    }
+
+  },
+
+  initActions() {
+
+    /* HAMBURGER */
+
+    function toggleMenu(visible) {
+      document.querySelector('.navlinks').classList.toggle('show', visible);
+      document.querySelector('.avatar-wrapper').classList.toggle('show', visible);
+
+    }
+
+    function toggleTopBar(visible) {
+        document.querySelector('.top-side').classList.toggle('display', visible);
+    }
+
+    document.querySelector('.hamburger').addEventListener('click', function(e) {
+      e.preventDefault();
+
+      toggleMenu();
+      console.log("clicked!");
+    });
+
+    document.querySelector('.hamburger').addEventListener('click', function(e) {
+      e.preventDefault();
+
+      toggleTopBar();
+    });
+
+    function closeModal() {
+      document.getElementById('overlay').classList.remove('show')
+    };
+
+    /* MODAL */
+
+    document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
+      btn.addEventListener('click', function(e) {
+        e.preventDefault()
+        closeModal()
+      })
+    });
+
+    document.querySelector('#overlay').addEventListener('click', function(e) {
+      if(e.target === this) {
+        closeModal()
+      }
+    });
+
+    document.addEventListener('keyup', function(e) {
+      if(e.keyCode === 27) {
+        closeModal()
+      }
+    });
+
+    function openModal(modal) {
+      document.querySelectorAll('#overlay > *').forEach(function(modal) {
+        modal.classList.remove('show')
+      })
+      document.querySelector('#overlay').classList.add('show')
+      document.querySelector(modal).classList.add('show')
+    };
+
+    window.addEventListener('beforeunload', function(e) {
+      e.preventDefault();
+
+      e.returnValue = false;
+
+      openModal('#myModal');
+    });
+
+  },
+
+};
+
+app.init();
---
+++ src/js/settings.js
@@ -0,0 +1,22 @@
+export const select = {
+  containerOf: {
+    pages: '#pages',
+    nav: '.sidebar',
+  },
+
+  nav: {
+    links: '.navlinks a',
+  },
+
+};
+
+export const classNames = {
+  nav: {
+    active: 'active',
+  },
+
+  pages: {
+    active: 'active',
+  },
+
+};
